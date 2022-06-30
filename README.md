73f791f530f5aaee155cba3efe249e839fbeeba9
Janie <jane0nguyen@gmail.com>
2022-06-29 21:31:33 -0500

    what am i doing

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # skipify
 A code for determining which songs you skip the most so you can remove them from your playlists
+
---
+++ main.py
@@ -0,0 +1,10 @@
+def main():
+    print('Hello World')
+    print('Syed???')
+    syed = 'very cool'
+    print(syed)
+    
+if __name__ == '__main__':
+    main()
+   
+    
